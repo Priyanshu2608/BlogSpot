1b058f8618f48b6c1b7107732c6439cccb6d67ed
Priyanshu Vasudev <142427709+Priyanshu2608@users.noreply.github.com>
2025-05-27 22:08:06 +0530

    Refines header styles for improved readability
    Updates the header component to enhance visual clarity and user experience.
    
    - Changes background color and text colors for better contrast.
    - Adjusts hover effects for navigation links.
    - Modifies search input styles for a more modern appearance.
    - Standardizes button text casing for consistency.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,25 +3,25 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <nav className='flex items-center justify-between border-b border-gray-200  bg-gradient-to-t from-blue-900 to-indigo-900 px-8 py-3'>
+    <nav className='flex items-center justify-between border-b border-gray-200  bg-gray-100 py-3'>
       <Link to='/' className='text-sm sm:text-xl'>
-        <span className='font-semibold text-white font-serif'>Blog Website</span>
+        <span className='font-bold px-2 text-blue-700 font-serif'>Blog Website</span>
       </Link>
-      <ul className='flex gap-8 text-white justify-center ml-[200px] font-bold '>
-        <Link to='/'><li className='hover:text-blue-950'>Home</li></Link>
-        <Link to='/about'><li className='hover:text-blue-950'>About</li></Link>
-        <Link to='/project'><li className='hover:text-blue-950'>Project</li></Link>
+      <ul className='flex gap-8 text-blue-700 justify-center ml-[200px] font-bold '>
+        <Link to='/'><li className='hover:text-blue-500'>Home</li></Link>
+        <Link to='/about'><li className='hover:text-blue-500'>About</li></Link>
+        <Link to='/project'><li className='hover:text-blue-500'>Project</li></Link>
       </ul>
       <div className='flex items-center mr-[-250px] '>
         <input
           type='text'
           placeholder='Search'
-          className='px-4 py-1 rounded-md outline-none bg-white text-black'
+          className='px-4 py-1 rounded-lg border-1 border-blue-700 bg-white text-black'
         />
       </div>
       <div className='flex items-center hover:bg-blue-950 rounded-2xl'>
         <button className='cursor-pointer '>
-           <Link to='/signin'><span className='text-white font-semibold border-2  px-4 py-1 rounded-2xl'>SignIn</span></Link>
+           <Link to='/signin'><span className='text-blue-700 font-semibold border-2 mr-2 px-4 py-1 rounded-2xl'>Sign in</span></Link>
         </button>
       </div>
     </nav>
