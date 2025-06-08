b367b4857078dd6eab111a9e0bd61657e29bb739
Priyanshu Vasudev <142427709+Priyanshu2608@users.noreply.github.com>
2025-06-08 17:31:46 +0530

    Fix Private Route

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,20 @@
-import React, { use } from 'react'
-import { Outlet } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Dashboard = () => {
-  const{currentUser}= useSelector((state) => state.user);
-  return currentUser ? <Outlet/> : <Navigate to='/signin'/>
+  const location = useLocation();
+  const [tab,setTab]= useState('')
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabfromUrl = urlParams.get('tab');
+    console.log(tabfromUrl);
+  },[location.search]);
+  return (
+    <div>
+      dash
+    </div>
+  )
 }
 
 export default Dashboard
+
